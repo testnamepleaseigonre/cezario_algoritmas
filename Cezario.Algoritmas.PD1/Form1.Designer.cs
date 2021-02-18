088d03afa658d395744a282c15a7a6f7a3cf2f3e
Valdemar <66011411+testnamepleaseigonre@users.noreply.github.com>
2021-02-18 13:01:51 +0200

    Add files via upload

---
+++ Cezario.Algoritmas.PD1/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace Cezario.Algoritmas.PD1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.poslinkisTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tekstasTextBox = new System.Windows.Forms.RichTextBox();
+            this.uzsifruotasTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.uzsifruotasTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tekstasTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.poslinkisTextBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cezario algoritmas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Poslinkis:";
+            // 
+            // poslinkisTextBox
+            // 
+            this.poslinkisTextBox.Location = new System.Drawing.Point(31, 131);
+            this.poslinkisTextBox.Name = "poslinkisTextBox";
+            this.poslinkisTextBox.Size = new System.Drawing.Size(75, 20);
+            this.poslinkisTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tekstas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Užšifruotas tekstas:";
+            // 
+            // tekstasTextBox
+            // 
+            this.tekstasTextBox.Location = new System.Drawing.Point(31, 170);
+            this.tekstasTextBox.Name = "tekstasTextBox";
+            this.tekstasTextBox.Size = new System.Drawing.Size(320, 96);
+            this.tekstasTextBox.TabIndex = 6;
+            this.tekstasTextBox.Text = "";
+            // 
+            // uzsifruotasTextBox
+            // 
+            this.uzsifruotasTextBox.Location = new System.Drawing.Point(31, 285);
+            this.uzsifruotasTextBox.Name = "uzsifruotasTextBox";
+            this.uzsifruotasTextBox.ReadOnly = true;
+            this.uzsifruotasTextBox.Size = new System.Drawing.Size(320, 96);
+            this.uzsifruotasTextBox.TabIndex = 7;
+            this.uzsifruotasTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Šifruoti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(754, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(693, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Dešifruoti";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // uzsifruotasTextBox2
+            // 
+            this.uzsifruotasTextBox2.Location = new System.Drawing.Point(448, 285);
+            this.uzsifruotasTextBox2.Name = "uzsifruotasTextBox2";
+            this.uzsifruotasTextBox2.ReadOnly = true;
+            this.uzsifruotasTextBox2.Size = new System.Drawing.Size(320, 96);
+            this.uzsifruotasTextBox2.TabIndex = 15;
+            this.uzsifruotasTextBox2.Text = "";
+            // 
+            // tekstasTextBox2
+            // 
+            this.tekstasTextBox2.Location = new System.Drawing.Point(448, 170);
+            this.tekstasTextBox2.Name = "tekstasTextBox2";
+            this.tekstasTextBox2.Size = new System.Drawing.Size(320, 96);
+            this.tekstasTextBox2.TabIndex = 14;
+            this.tekstasTextBox2.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(445, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Dešifruotas tekstas:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(445, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Užšifruotas tekstas:";
+            // 
+            // poslinkisTextBox2
+            // 
+            this.poslinkisTextBox2.Location = new System.Drawing.Point(448, 131);
+            this.poslinkisTextBox2.Name = "poslinkisTextBox2";
+            this.poslinkisTextBox2.Size = new System.Drawing.Size(75, 20);
+            this.poslinkisTextBox2.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(445, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Poslinkis:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(114, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(166, 37);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Šifravimas";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(517, 64);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(201, 37);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Dešifravimas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.uzsifruotasTextBox2);
+            this.Controls.Add(this.tekstasTextBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.poslinkisTextBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.uzsifruotasTextBox);
+            this.Controls.Add(this.tekstasTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.poslinkisTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox poslinkisTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox tekstasTextBox;
+        private System.Windows.Forms.RichTextBox uzsifruotasTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox uzsifruotasTextBox2;
+        private System.Windows.Forms.RichTextBox tekstasTextBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox poslinkisTextBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Cezario.Algoritmas.PD1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cezario.Algoritmas.PD1
+{
+    public partial class Form1 : Form
+    {
+        private int symbolsCount = 128;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                checkIfDataValid(poslinkisTextBox.Text.ToString(), tekstasTextBox.Text.ToString());
+                tekstoSifravimas(int.Parse(poslinkisTextBox.Text.ToString()), tekstasTextBox.Text.ToString()) ;
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void checkIfDataValid(string poslinkis, string tekstas)
+        {
+            if (string.IsNullOrEmpty(poslinkis) || string.IsNullOrEmpty(tekstas))
+                throw new Exception("Užpildykite laukus!");
+            try
+            {
+                int.Parse(poslinkis);
+            }
+            catch
+            {
+                throw new Exception("Poslinkis turi buti skaiciumi!");
+            }
+            if(int.Parse(poslinkis) < 1 || int.Parse(poslinkis) > 127)
+                throw new Exception("Poslinkis turi buti skaiciumi (1-127)!");
+        }
+
+        private void tekstoSifravimas(int poslinkis, string tekstas)
+        {
+            string uzsifruotasTekstas = null;
+            List<int> textIntList = getTextIntList(tekstas);
+            for (int i = 0; i < tekstas.Length; i++)
+            {
+                uzsifruotasTekstas += (char)((textIntList[i] + poslinkis) % symbolsCount);
+            }
+            uzsifruotasTextBox.Text = uzsifruotasTekstas;
+        }
+
+        private void tekstoDesifravimas(int poslinkis, string tekstas)
+        {
+            string desifruotasTekstas = null;
+            List<int> textIntList = getTextIntList(tekstas);
+            for (int i = 0; i < tekstas.Length; i++)
+            {
+                desifruotasTekstas += (char)(((textIntList[i] - poslinkis) + symbolsCount) % symbolsCount);
+            }
+            uzsifruotasTextBox2.Text = desifruotasTekstas;
+        }
+
+        private List<int> getTextIntList(string tekstas)
+        {
+            List<int> returnList = new List<int>();
+            foreach (char ch in tekstas)
+            {
+                returnList.Add(ch);
+            }
+            return returnList;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                checkIfDataValid(poslinkisTextBox2.Text.ToString(), tekstasTextBox2.Text.ToString());
+                tekstoDesifravimas(int.Parse(poslinkisTextBox2.Text.ToString()), tekstasTextBox2.Text.ToString());
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
